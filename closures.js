b5973c0f6da7c909a26ada5e9b66093a6de16c30
Amantina Sosa <amantinasosa@gmail.com>
2019-09-18 13:35:17 -0400

    complete

--- closures.js
+++ closures.js
@@ -1,4 +1,4 @@
-/* 
+/*
   Once you complete a problem, refresh ./closures.html in your browser and check to see if the problem's test(s) are passing.
   Passed tests will be indicated by a green circle.
   Failed tests will be indicated by a red X.
@@ -16,21 +16,23 @@ function outer() {
   };
 }
 // Do not edit the code above.
-  
-/* 
+
+/*
   Above you're given a function that returns another function which has a closure over the name variable.
   Invoke outer saving the return value into another variable called 'inner'.
 */
-  
-// Code Here
 
+// Code Here
 
 
 //Once you do that, invoke inner.
 
 //Code Here
-
-
+function inner (func){
+  return function () {
+    return func()
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -46,14 +48,14 @@ function callFriend(name) {
 /*
   Above you're given a callFriend function that returns the dial function.
   Store the result of invoking callFriend in a variable named callJake.
-  
-  When callJake is invoked with '435-555-9248', it returns 'Calling Jake at 435-555-9248' 
+
+  When callJake is invoked with '435-555-9248', it returns 'Calling Jake at 435-555-9248'
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
 //Code Here
-
-
+//just invoke and store
+let callJake = callFriend('Jake')
 
 ////////// PROBLEM 3 //////////
 
@@ -62,16 +64,21 @@ function callFriend(name) {
 */
 
 //Code Here
-
-
+function makeCounter () {
+  let count = 0
+  return function () {
+    count++
+    //always remember to return
+    return count
+  }
+}
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
-
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 ////////// PROBLEM 4 //////////
@@ -81,45 +88,54 @@ function callFriend(name) {
   The first function is called inc, this function is responsible for incrementing the value once and returning the updated value.
   The second function is called dec, this function is responsible for decrementing the value by one and returning the updated value.
   You will need to use the module pattern to achieve this.
-  Information on the module pattern available here: 
+  Information on the module pattern available here:
   http://stackoverflow.com/questions/17776940/javascript-module-pattern-with-example?answertab=votes#tab-top
 */
 
 function counterFactory(value) {
-  // Code here.
-
-  return {
-
-  };
+  //this increases
+function inc () {
+  value++
+  return value
+}
+  //this decreases
+function dec () {
+  value--
+  return value
+}
+  //this is the callback
+return {
+  inc,
+  dec
+}
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
-
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 ////////// PROBLEM 5 //////////
 
 /*
   Inside the motivation function create another function called message that will return the welcome text with the firstname and lastname.
-  The final message should say "You're doing awesome, keep it up firstname lastname." 
+  The final message should say "You're doing awesome, keep it up firstname lastname."
   (Hint: don't forget to have a space between the firstname and lastname and a period at the end of the sentence.)
 */
 
 function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
-  // code message function here.
+  function message (){
+  return `You're doing awesome, keep it up ${firstname} ${lastname}.`
+  }
 
-  //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
-
 
 
 ////////// PROBLEM 6 //////////
@@ -131,22 +147,26 @@ var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up B
 
 var module = (function() {
   var person = {
-    name: "phillip",
+    name: 'phillip',
     age: 29,
-    location: "Utah"
+    location: 'Utah'
   };
 
   function privateMethod(){
-    return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
+    return "Hi, I'm " + person.name + ', age ' + person.age + ' from ' + person.location;
   }
 
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    //this is a property
+    publicMethod: function () {
+      //it invokes private to get the result
+      let result = privateMethod()
+      return result
+    }
   };
 })();
-
 
 
 ////////// PROBLEM 7 //////////
@@ -163,13 +183,19 @@ function secretNumber() {
 
   return {
     // Code here
+    //they tell you it takes something in the parameter
+    addToSecret: function (value) {
+      return secret += value
+    },
+    takeAwayFromSecret: function (value) {
+      return secret -= value
+    }
   };
 }
 
 
-
 ////////// PROBLEM 8 //////////
-  
+
 /*
   Here we have a for loop that will iterate as long as i is less than or equal to 5.
   What we need to do is console.log(i) so that it logs like so:
@@ -182,15 +208,27 @@ function secretNumber() {
 
   However, because each call to console.log occurs after the loop has finished, the value of i has changed before the console.log executes.
   We'll need to use a closure to preserve a reference to i at the time of execution.
-  
+
   Fix the code below to log the desired output.
 */
 
+// function timeOutCounter() {
+//   for (var i = 0; i <= 5; i++) {
+//     setTimeout(function() {
+//       console.log(i);
+//     }, i * 1000);
+//   }
+// }
+// timeOutCounter();
+
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
+  function newFun(i){
     setTimeout(function() {
       console.log(i);
     }, i * 1000);
+
   }
-}
-timeOutCounter();
+    for (let i = 0; i <= 5; i++) {newFun(i)
+    }
+  }
+  timeOutCounter();
